648c3bbf5baba9f1343cc1fd64c064bb6306ada0
Arika <Karika21@tbc.edu.np>
2022-10-21 16:03:31 +0545

    modelassignment3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\backend\PostController;
+use App\Http\Controllers\backend\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('category-recycles',[CategoryController::class,'recycle'])->name('category.recycle');
+Route::post('restore/{id}', [CategoryController::class, 'restore'])->name('category.restore');
+Route::delete('permanent/delete/{id}', [CategoryController::class, 'forceDelete'])->name('category.forceDelete');
+Route::resource('category',CategoryController::class);
+
+Route::get('post-recycles',[PostController::class,'recycle'])->name('post.recycle');
+Route::post('restore-post/{id}', [PostController::class, 'restore'])->name('post.restore');
+Route::delete('permanent-post/delete/{id}', [PostController::class, 'forceDelete'])->name('post.forceDelete');
+Route::resource('post',PostController::class);
